24cbe39e03dc4cbcd4990a2686a48986bbd53af3
Iyed BEN REJEB <iyed.exe@gmail.com>
2025-08-10 19:30:17 +0200

    Definition of broker message handlers.

--- src/fix/Broker.cpp
+++ src/fix/Broker.cpp
@@ -3,9 +3,73 @@
 
 namespace BNB::FIX
 {
-    void Broker::onMessage(const FIX44::OE::Heartbeat& message, const SessionID& sessionID)
+    void Broker::onMessage(const FIX44::OE::ExecutionReport &message, const SessionID &sessionID)
+    {
+        LOG_WARNING("Received unhandled ExecutionReport: [{}]", replaceSOHWithPipe(message.toString()));
+    }
+    void Broker::onMessage(const FIX44::OE::Heartbeat &message, const SessionID &sessionID)
     {
         LOG_WARNING("Received unhandled Heartbeat: [{}]", replaceSOHWithPipe(message.toString()));
+    }
+    void Broker::onMessage(const FIX44::OE::LimitQuery &message, const SessionID &sessionID)
+    {
+        LOG_WARNING("Received unhandled LimitQuery: [{}]", replaceSOHWithPipe(message.toString()));
+    }
+    void Broker::onMessage(const FIX44::OE::LimitResponse &message, const SessionID &sessionID)
+    {
+        LOG_WARNING("Received unhandled LimitResponse: [{}]", replaceSOHWithPipe(message.toString()));
+    }
+    void Broker::onMessage(const FIX44::OE::ListStatus &message, const SessionID &sessionID)
+    {
+        LOG_WARNING("Received unhandled ListStatus: [{}]", replaceSOHWithPipe(message.toString()));
+    }
+    void Broker::onMessage(const FIX44::OE::Logon &message, const SessionID &sessionID)
+    {
+        LOG_WARNING("Received unhandled Logon: [{}]", replaceSOHWithPipe(message.toString()));
+    }
+    void Broker::onMessage(const FIX44::OE::Logout &message, const SessionID &sessionID)
+    {
+        LOG_WARNING("Received unhandled Logout: [{}]", replaceSOHWithPipe(message.toString()));
+    }
+    void Broker::onMessage(const FIX44::OE::NewOrderList &message, const SessionID &sessionID)
+    {
+        LOG_WARNING("Received unhandled NewOrderList: [{}]", replaceSOHWithPipe(message.toString()));
+    }
+    void Broker::onMessage(const FIX44::OE::NewOrderSingle &message, const SessionID &sessionID)
+    {
+        LOG_WARNING("Received unhandled NewOrderSingle: [{}]", replaceSOHWithPipe(message.toString()));
+    }
+    void Broker::onMessage(const FIX44::OE::News &message, const SessionID &sessionID)
+    {
+        LOG_WARNING("Received unhandled News: [{}]", replaceSOHWithPipe(message.toString()));
+    }
+    void Broker::onMessage(const FIX44::OE::OrderAmendKeepPriorityRequest &message, const SessionID &sessionID)
+    {
+        LOG_WARNING("Received unhandled OrderAmendKeepPriorityRequest: [{}]", replaceSOHWithPipe(message.toString()));
+    }
+    void Broker::onMessage(const FIX44::OE::OrderAmendReject &message, const SessionID &sessionID)
+    {
+        LOG_WARNING("Received unhandled OrderAmendReject: [{}]", replaceSOHWithPipe(message.toString()));
+    }
+    void Broker::onMessage(const FIX44::OE::OrderCancelReject &message, const SessionID &sessionID)
+    {
+        LOG_WARNING("Received unhandled OrderCancelReject: [{}]", replaceSOHWithPipe(message.toString()));
+    }
+    void Broker::onMessage(const FIX44::OE::OrderCancelRequest &message, const SessionID &sessionID)
+    {
+        LOG_WARNING("Received unhandled OrderCancelRequest: [{}]", replaceSOHWithPipe(message.toString()));
+    }
+    void Broker::onMessage(const FIX44::OE::OrderCancelRequestAndNewOrderSingle &message, const SessionID &sessionID)
+    {
+        LOG_WARNING("Received unhandled OrderCancelRequestAndNewOrderSingle: [{}]", replaceSOHWithPipe(message.toString()));
+    }
+    void Broker::onMessage(const FIX44::OE::OrderMassCancelReport &message, const SessionID &sessionID)
+    {
+        LOG_WARNING("Received unhandled OrderMassCancelReport: [{}]", replaceSOHWithPipe(message.toString()));
+    }
+    void Broker::onMessage(const FIX44::OE::OrderMassCancelRequest &message, const SessionID &sessionID)
+    {
+        LOG_WARNING("Received unhandled OrderMassCancelRequest: [{}]", replaceSOHWithPipe(message.toString()));
     }
 
 }
